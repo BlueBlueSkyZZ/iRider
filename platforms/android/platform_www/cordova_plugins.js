ecae658f92662b118ec537c8f9cb645dbd162ae7
BlueBlueSkyZZ <493813966@qq.com>
2017-04-07 22:39:57 +0800

    show version

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -356,6 +356,44 @@ module.exports = [
         "clobbers": [
             "ble"
         ]
+    },
+    {
+        "id": "cordova-plugin-device-orientation.CompassError",
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device-orientation.CompassHeading",
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassHeading.js",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device-orientation.compass",
+        "file": "plugins/cordova-plugin-device-orientation/www/compass.js",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.geolocation",
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.PositionError",
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "runs": true
     }
 ];
 module.exports.metadata = 
@@ -372,7 +410,9 @@ module.exports.metadata =
     "cordova-plugin-file-transfer": "1.6.3-dev",
     "cordova-plugin-dialogs": "1.3.2",
     "cordova-plugin-vibration": "2.1.4",
-    "cordova-plugin-ble-central": "1.1.4"
+    "cordova-plugin-ble-central": "1.1.4",
+    "cordova-plugin-device-orientation": "1.0.6",
+    "cordova-plugin-geolocation": "2.4.2"
 };
 // BOTTOM OF METADATA
 });
